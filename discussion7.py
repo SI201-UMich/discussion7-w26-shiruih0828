83c09b205246f779e84ca3cf1fb987140dfe9941
Shirley Huang <shirleyhuang@ShirleydeMacBook-Air-7.local>
2026-02-25 18:09:24 -0500

    task3

--- discussion7.py
+++ discussion7.py
@@ -77,7 +77,7 @@ def calculate_avg_price_by_neighbourhood_group_and_room(listings):
         agg[key]["total"] += price
         agg[key]["count"] += 1
 
-        
+
     averages = {}
     for key, stats in agg.items():
         if stats["count"] > 0:
@@ -107,6 +107,24 @@ def write_summary_csv(out_filename, avg_prices):
         None
             Writes a CSV file with header: neighbourhood_group, room_type, average_price
     """
+    with open(out_filename, "w", newline="", encoding="utf-8") as f:
+
+        writer = csv.DictWriter(
+            f,
+            fieldnames=["neighbourhood_group", "room_type", "average_price"]
+        )
+
+        writer.writeheader()
+
+        for (ng, rt), avg in avg_prices.items():
+            writer.writerow({
+                "neighbourhood_group": ng,
+                "room_type": rt,
+                "average_price": avg
+            })
+
+               
+
     pass
 
 ###############################################################################
